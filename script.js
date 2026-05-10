1aa28ad0b1b9b7beb6d60094d0852ffa6ae543e8
Vijay <192565102.simats@saveetha.com>
2026-05-10 16:43:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 const input = document.getElementById("userInput");
 const chatBox = document.getElementById("chatBox");
-
-function sendMessage(){
+const API_KEY = "AIzaSyDoONDq0717FuigN7g8wFQqlvQOUwzNPjA";
+async function sendMessage() {
   const text = input.value.trim();
-  if(!text) return;
-
+  if (!text) return;
   const userMsg = document.createElement("div");
   userMsg.className = "message user";
   userMsg.innerHTML = `<span>${text}</span>`;
@@ -13,37 +12,97 @@ function sendMessage(){
 
   input.value = "";
 
-  setTimeout(()=>{
-    const aiMsg = document.createElement("div");
-    aiMsg.className = "message ai";
-    aiMsg.innerHTML = `<span>⚡ Vijzzz AI received: "${text}"</span>`;
-    chatBox.appendChild(aiMsg);
+  const aiMsg = document.createElement("div");
+
+  aiMsg.className = "message ai";
+
+  aiMsg.innerHTML = `<span>⚡ Vijay AI typing...</span>`;
+
+  chatBox.appendChild(aiMsg);
+
+  chatBox.scrollTop = chatBox.scrollHeight;
+
+  try {
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: text
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+    const data = await response.json();
+    const reply =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "No response";
+    aiMsg.innerHTML = `<span>${reply}</span>`;
     chatBox.scrollTop = chatBox.scrollHeight;
-  },700);
+
+  }
+
+  catch (error) {
+
+    aiMsg.innerHTML = `<span>⚠️ Error connecting AI</span>`;
+
+  }
+
 }
 
-input.addEventListener("keypress",function(e){
-  if(e.key==="Enter") sendMessage();
+input.addEventListener("keypress", function(e){
+
+  if(e.key === "Enter"){
+
+    sendMessage();
+
+  }
+
 });
 
 const voiceBtn = document.getElementById("voiceBtn");
 
 if ('webkitSpeechRecognition' in window) {
+
   const recognition = new webkitSpeechRecognition();
+
   recognition.continuous = false;
+
   recognition.lang = 'en-US';
 
   voiceBtn.onclick = () => {
+
     recognition.start();
+
     voiceBtn.innerText = "🎙 Listening...";
+
   };
 
   recognition.onresult = (event) => {
+
     input.value = event.results[0][0].transcript;
+
     voiceBtn.innerText = "🎤 Voice";
+
   };
 
   recognition.onerror = () => {
+
     voiceBtn.innerText = "🎤 Voice";
+
   };
+
 }
